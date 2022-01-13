f9547c29eba0993d22abef96a8abd04f6a501d1e
omlie <lieolemagnus@gmail.com>
2022-01-13 19:58:29 +0100

    Menu tweaks

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,6 +3,7 @@ import { TLink } from "~/types/shared";
 import { Layout } from ".";
 import Home from "remixicon-react/HomeHeartFillIcon";
 import Menu from "remixicon-react/MenuLineIcon";
+import Close from "remixicon-react/CloseLineIcon";
 import { useState } from "react";
 import clsx from "clsx";
 import { overrideTailwindClasses } from "tailwind-override";
@@ -20,33 +21,34 @@ const NavBar = ({ links }: { links: TLink[] }) => {
           >
             <Home size={40} />
           </NavLink>
-          <a
-            onClick={() => setOpen(!open)}
-            className="py-4 text-2xl text-pink-accent"
-          >
-            <Menu size={40} />
-          </a>
-        </div>
-        {open && (
-          <div
-            className={clsx(
-              "fixed right-0 flex flex-col bg-blue-dark min-w-[800px]",
-              {
-                ["h-screen"]: open,
-              }
-            )}
-          >
-            {links.map(({ text, slug }) => (
-              <NavLink
-                to={slug}
-                key={slug}
-                className="py-4 text-2xl text-pink-accent hover:text-orange-accent focus:text-orange-accent"
-              >
-                {text}
-              </NavLink>
-            ))}
+          <div className="relative flex flex-col items-end w-96">
+            <a
+              onClick={() => setOpen(!open)}
+              className="py-4 text-2xl text-pink-accent"
+            >
+              {!open ? <Menu size={40} /> : <Close size={40} />}
+            </a>
+            <div
+              className={clsx(
+                "absolute right-0 flex flex-col bg-blue-dark p-10 transition-all rounded-5xl translate-x-10 text-right",
+                {
+                  "h-auto -translate-y-full": !open,
+                  "mt-16 shadow-menu": open,
+                }
+              )}
+            >
+              {links.map(({ text, slug }) => (
+                <NavLink
+                  to={slug}
+                  key={slug}
+                  className="py-4 text-2xl no-underline text-pink-accent hover:text-orange-accent focus:text-orange-accent rounded-3xl"
+                >
+                  {text}
+                </NavLink>
+              ))}
+            </div>
           </div>
-        )}
+        </div>
       </Layout>
     </nav>
   );
